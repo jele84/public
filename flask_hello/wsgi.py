313abc8edd3f4545205c127c6afca2cd08227834
jele84 <jele@yandex.ru>
2022-09-13 17:28:53 +0000

    some fixes

--- flask_hello/wsgi.py
+++ flask_hello/wsgi.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+	return 'Hello, anonymous!'
+
+
 @app.route('/<string:name>')
 def greeting(name: str):
-	return f'Hello, {name.capitalize()}'
+	return f'Hello, {name.capitalize()}!'
 
 if __name__ == '__main__':
 	app.run()
